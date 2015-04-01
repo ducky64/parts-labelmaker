469d244bec3b6ddfa10a481be332112597be2d0a
ducky64 <richard.lin@berkeley.edu>
2015-04-01 11:43:11 -0700

    rant rant rant

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # parts-labelmaker
-Label generator for electronic parts. Complete spreadsheet-to-labels functionality
+Label generator for electronic parts. Complete spreadsheet-to-labels functionality.
+
+Dependent Python packages:
+For DigiKey SWS Interface (to get component data):
+  pysimplesoap
+  httplib2
+  A magical, not-documented-at-all PartnerID. Come on, DigiKey, if you're going to have web services, at least document it properly and open it up. In the mean time, we'll just hog your bandwidth by requesting the whole component webpage. Good job, guys.
--- digikey-sws-interface.py
+++ digikey-sws-interface.py
@@ -1,6 +1,28 @@
+import configparser
+
 from pysimplesoap.client import SoapClient
 
-class DigiKeySwsInterface(object):
-  def __init__(self):
-    self.client = SoapClient(wsdl="http://services.digikey.com/search/search.asmx?wsdl",trace=False)
 
+
+class DigiKeySwsInterface(object):
+  CONFIG_SECTION = 'DigiKeySwsInterface'
+  def __init__(self):
+    config = configparser.RawConfigParser()
+    config.read('config.cfg')
+
+    self.client = SoapClient(wsdl='http://services.digikey.com/search/search.asmx?wsdl',
+                             trace=True)
+    self.client['wsse:Security'] = {'wsse:UsernameToken': 
+                                    {'wsse:Username': config.get(self.CONFIG_SECTION, 'Username'), 
+                                     'wsse:Password': config.get(self.CONFIG_SECTION, 'Password')}}
+    self.client['PartnerInformation'] = {'PartnerId': config.get(self.CONFIG_SECTION, 'PartnerId')}
+
+  def get_component_parameters(self, components):
+    output = {}
+    for component in components:
+      output[component] = self.client.GetProductInfoByDigikeyPartNumber(digikeyPartNumber=component)
+  
+if __name__ == '__main__':
+  dksi = DigiKeySwsInterface()
+  print(dksi.get_component_parameters(['NDP7060-ND']))
+  
