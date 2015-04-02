d70b958704feedafe7274967468bb76ce563ee60
Richard Lin <richard.lin@berkeley.edu>
2015-04-01 17:51:54 -0700

    Refactor CsvRewriter

--- Annotator.py
+++ Annotator.py
@@ -30,51 +30,35 @@ if __name__ == '__main__':
   # dict of (Supplier, SupplierPartNumber) to part parametrics dict
   parametric_data = {}
   
-  output_filename = args.filename + OUTPUT_POSTFIX + '.csv'
   input_filename = args.filename + '.csv'
+  output_filename = args.filename + OUTPUT_POSTFIX + '.csv'
+  
+  if not os.path.isfile(input_filename):
+    raise AnnotatorError("Input CSV '%s' not found" % input_filename)
+  
+  def process_fn(key_dict):
+    supplier = key_dict['Supplier']
+    supplierpn = key_dict['SupplierPartNumber']
+    
+    if supplier in SupplierConfig.SupplierConfig:
+      print("Fetching '%s' from '%s'" % (supplierpn, supplier))
+      fetcher = SupplierConfig.SupplierConfig[supplier][0]
+      parametrics = fetcher.get_component_parametrics(supplierpn)
+      return {'SupplierParametrics': Common.parametric_to_string(parametrics)}
+    else:
+      return {}
+          
+  rewriter = Common.CsvRewriter(['Supplier', 'SupplierPartNumber'], 
+                                ['SupplierParametrics'], 
+                                process_fn)
   
   if os.path.isfile(output_filename):
     print("Existing parametric CSV file detected, reading in data")
-    with open(output_filename) as csvfile:
-      reader = csv.DictReader(csvfile)
-      fields = reader.fieldnames
-      if 'Supplier' not in fields or 'SupplierPartNumber' not in fields:
-        # This is a crash to prevent overwriting existing data
-        raise AnnotatorError("Invalid existing parametric CSV")
-      for row in reader:
-        parametric_data[(row['Supplier'], row['SupplierPartNumber'])] = row['SupplierParametrics']
-  else:
-    print("Existing parametric CSV file not detected")
+    rewriter.read_output_csv(output_filename)
     
-  if not os.path.isfile(input_filename):
-    raise AnnotatorError("Input CSV '%s' not found" % input_filename)
+  rewriter.read_input_csv(input_filename)
+  
+  rewriter.write_output_csv(output_filename)  
     
-  with open(input_filename) as csvfile, open(output_filename, 'w') as outfile:
-    reader = csv.DictReader(csvfile)
-    fields = reader.fieldnames
-    if 'Supplier' not in fields:
-      raise AnnotatorError("Supplier not in input CSV '%s'" % input_filename)
-    if 'SupplierPartNumber' not in fields:  
-      raise AnnotatorError("SupplierPartNumber not in input CSV '%s'" % input_filename)
-    if 'SupplierParametrics' in fields:
-      raise AnnotatorError("SupplierParametrics in input CSV '%s'" % input_filename)
-    
-    fields.append('SupplierParametrics')
-    writer = csv.DictWriter(outfile, fieldnames=fields)
-    writer.writeheader()
-    for row in reader:
-      supplier = row['Supplier']
-      supplierpn = row['SupplierPartNumber']
-      parametric_key = (supplier, supplierpn)
-      if parametric_key in parametric_data:
-        row['SupplierParametrics'] = parametric_data[parametric_key]
-      else:
-        if supplier in SupplierConfig.SupplierConfig:
-          print("Fetching '%s' from '%s'" % (supplierpn, supplier))
-          fetcher = SupplierConfig.SupplierConfig[supplier][0]
-          parametrics = fetcher.get_component_parametrics(supplierpn)
-          row['SupplierParametrics'] = Common.parametric_to_string(parametrics)
-          
-      writer.writerow(row)
-      
-print("Done")
+  print("Done")
+  
--- Common.py
+++ Common.py
@@ -1,3 +1,5 @@
+import csv
+
 def parametric_to_string(parametric_dict):
   out = ""
   for key, val in parametric_dict.items():
@@ -7,3 +9,67 @@ def parametric_to_string(parametric_dict):
       out += ";"
     out += key + "=" + val
   return out
+
+class CsvSanityError(Exception):
+  pass
+
+class CsvRewriter(object):
+  def __init__(self, key_fields, value_fields, process_fn):
+    self.fieldnames = None
+    self.key_fields = key_fields
+    self.value_fields = value_fields
+    self.process_fn = process_fn
+    
+    self.in_data = [] # raw rowdict from input
+    self.out_keyed_data = {} # key_fields tuple to {value_key: value_value} dict 
+    
+  def read_input_csv(self, csv_filename):
+    with open(csv_filename) as csvfile:
+      reader = csv.DictReader(csvfile)
+      fields = reader.fieldnames
+      if self.fieldnames is None:
+        self.fieldnames = fields
+      else:
+        assert(self.fieldnames == fields)
+        
+      if not all([elt in fields for elt in self.key_fields]):
+        raise CsvSanityError("Input '%s' missing key fields" % csv_filename)
+      for row in reader:
+        self.in_data.append(row)
+      
+  def read_output_csv(self, csv_filename):
+    with open(csv_filename) as csvfile:
+      reader = csv.DictReader(csvfile)
+      fields = reader.fieldnames
+      if not all([elt in fields for elt in self.key_fields]):
+        raise CsvSanityError("Output '%s' missing key fields" % csv_filename)
+      if not all([elt in fields for elt in self.value_fields]):
+        raise CsvSanityError("Output '%s' missing value fields" % csv_filename)
+      for row in reader:
+        keys = tuple([row[key_field] for key_field in self.key_fields])
+        values = {key: row[key] for key in self.value_fields}
+        assert keys not in self.out_keyed_data
+        self.out_keyed_data[keys] = values
+              
+  def write_output_csv(self, csv_filename):
+    existing_count = 0
+    processed_count = 0
+    with open(csv_filename, 'w') as csvfile:
+      writer = csv.DictWriter(csvfile,
+                              fieldnames=self.fieldnames+self.value_fields)
+      writer.writeheader()
+      
+      for row in self.in_data:
+        keys = tuple([row[key_field] for key_field in self.key_fields])
+        keys_dict = dict(zip(self.key_fields, keys))
+        if keys in self.out_keyed_data:
+          values = self.out_keyed_data[keys]
+          existing_count += 1
+        else:
+          values = self.process_fn(keys_dict)
+          processed_count += 1
+        row.update(values)
+        writer.writerow(row)
+        
+      print("%i existing entries, %i newly processed entries" 
+            % (existing_count, processed_count))
